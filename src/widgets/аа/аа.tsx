f0923b75d28a4de1cfb3232677003e10277cb1eb
Harconnen <harconnen.public@gmail.com>
2023-05-16 11:00:50 +0300

    Сохраняйте спокойствие и действуйте - 3

--- src/widgets/аа/аа.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './style.module.css';
-
-type Props = {};
-
-export const аа: React.FC<Props> = (props) => {
-  return <div className={styles.container} data-testid="test" />;
-};
