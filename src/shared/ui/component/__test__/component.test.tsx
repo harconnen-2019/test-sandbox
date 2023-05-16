7484ab8c630a2e9ae13e073ce07f886de7f86ef1
Harconnen <harconnen.public@gmail.com>
2023-05-16 10:35:34 +0300

    Сохраняйте спокойствие и действуйте

--- src/shared/ui/component/__test__/component.test.tsx
+++ src/shared/ui/component/__test__/component.test.tsx
@@ -5,7 +5,7 @@ import { Component } from '../component';
 describe('/shared/ui/component', () => {
   it('Название', () => {
     render(<Component>test</Component>);
-    const element = screen.getByTestId(/Component1/i);
+    const element = screen.getByTestId(/Component/i);
     expect(element).toBeInTheDocument();
     expect(element).toMatchSnapshot();
   });
